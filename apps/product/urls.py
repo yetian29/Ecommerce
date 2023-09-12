87ebe879867ae82bb8799d317a5ad9e89d9dfe9d
yetian29 <dathien000@gmail.com>
2023-09-13 06:32:52 +0700

    Add

--- apps/product/urls.py
+++ apps/product/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from .views import ProductListView, ProductDetailView, ProductListCategoryView, ProductBySearchView
+from .views import ProductListView, ProductDetailView, ProductListCategoryView, ProductBySearchView, SearchView
 
 urlpatterns = [
    path('', ProductListView.as_view()),
    path('<product_id>', ProductDetailView.as_view()),
    path('category/<category_id>', ProductListCategoryView.as_view()),
-   path('by/search', ProductBySearchView.as_view())
+   path('by/search', ProductBySearchView.as_view()),
+   path('search/<search_term>',SearchView.as_view() )
 
 ]
--- apps/product/views.py
+++ apps/product/views.py
@@ -6,7 +6,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import permissions
 from apps.category.models import Category
-
+from django.db.models import Q
 # Create your views here.
 
 
@@ -58,7 +58,7 @@ class ProductListCategoryView(APIView):
                 if category.products.all().exists():
                     products = category.products.all()
                     products = ProductSerializer(products, many=True)
-                    return Response({'products': products.data}, status=status.HTTP_200_OK)
+                    return Response({'filtered_products': products.data}, status=status.HTTP_200_OK)
                 return Response({'Error': 'No products found'}, status=status.HTTP_404_NOT_FOUND)
             return Response({'Error': 'Category does not exist'}, status=status.HTTP_404_NOT_FOUND)
 
@@ -113,4 +113,25 @@ class ProductBySearchView(APIView):
                 return Response({'Error': 'No products found'}, status=status.HTTP_404_NOT_FOUND)
             return Response({'Error': 'No category found'}, status=status.HTTP_404_NOT_FOUND)
                        
+
+class SearchView(APIView):
+    def get(self, request, search_term, format=None):
+
+        try:
+            search_term = int(search_term)
+        except:
+            products = Product.objects.filter(
+            Q(name__icontains=search_term) |
+            Q(description__icontains=search_term) |
+            Q(category__name__icontains=search_term) 
+
+            )
+            if products:
+                products = ProductSerializer(products, many=True)
+                return Response({'searched_products': products.data}, status=status.HTTP_200_OK)
+            return Response({'Error': 'No products found'}, status=status.HTTP_404_NOT_FOUND)
+
+        else:
+            return Response({'Error': 'Search Term invalid. Search Term must be string'}, status=status.HTTP_400_BAD_REQUEST)
+        
 
