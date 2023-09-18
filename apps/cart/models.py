5ad7484b237cc8ad7bb5cb93c07b849a4755eb82
yetian29 <125103035+yetian29@users.noreply.github.com>
2023-09-18 06:58:14 +0000

    Mon, Sep 18, 2023, 1:58 PM +07:00

--- apps/cart/models.py
+++ apps/cart/models.py
@@ -8,16 +8,17 @@ User = settings.AUTH_USER_MODEL
 
 
 class Cart(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='cart')
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     total_items = models.IntegerField(default=0)
 
 
   
-class CartItem(models.Model):
+class CartItem(models.Model): 
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='cart_items')
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='cart_items')
     count = models.IntegerField(default=0)
 
+    
         
 
     
