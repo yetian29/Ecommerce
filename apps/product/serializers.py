e1917d3a2a21425330af7cd6d44e89a11416dd85
yetian29 <125103035+yetian29@users.noreply.github.com>
2023-09-18 07:12:02 +0000

    Mon, Sep 18, 2023, 2:12 PM +07:00

--- apps/product/serializers.py
+++ apps/product/serializers.py
@@ -3,13 +3,14 @@ from .models import Product
 
 
 class ProductSerializer(serializers.ModelSerializer):
+     
     class Meta:
         model = Product
         fields = "__all__"
 
 
 
-        
+
 
 
 
